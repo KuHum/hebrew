83d2142dc6da454e072dc70e2f9465c0f5ad62d6
KuHum <95577618+KuHum@users.noreply.github.com>
2026-06-06 13:42:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ============================================================
 // Service Worker for Aleph with Beth Tracker
 // ============================================================
-const CACHE_NAME = 'aleph-beth-v8';
+const CACHE_NAME = 'aleph-beth-v9';
 
 const STATIC_ASSETS = [
     './',
